513ef351c34bfa0068d7e7d28f7f53ac63e99749
abhishek malvadkar ranoji <abhishek@dev.com>
2021-11-20 18:41:57 +0530

    Created new spring boot project

---
+++ source/persistence-layer-practice/src/main/java/com/practice/jpa/hibernate/PersistenceLayerPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceLayerPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceLayerPracticeApplication.class, args);
+	}
+
+}
---
+++ source/persistence-layer-practice/src/test/java/com/practice/jpa/hibernate/PersistenceLayerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jpa.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceLayerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
